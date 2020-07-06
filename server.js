d1e5c35889d5d3bbf9b47b2139f149715e5e2022
kubila <dolaskyasin@gmail.com>
2020-07-06 23:53:10 +0300

    Delete done, CRUD completed

--- server.js
+++ server.js
@@ -87,5 +87,22 @@ app
   })
   // DELETE
   .delete((req, res) => {
-    // User.findByIdAndDelete()
+    User.findByIdAndDelete(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err,
+        });
+      } else if (!data) {
+        res.json({
+          success: false,
+          message: 'Not found',
+        });
+      } else {
+        res.json({
+          success: true,
+          data: data,
+        });
+      }
+    });
   });
